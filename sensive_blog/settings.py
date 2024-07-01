0760081a681c8b8e310ed43203129beaf98a072a
Aleksandr Skripko <skripkoae@gmail.com>
2024-07-01 11:48:58 +0300

    add DDT

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -20,11 +20,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 
     'blog',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'sensive_blog.wsgi.application'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 DATABASES = {
     'default': {
