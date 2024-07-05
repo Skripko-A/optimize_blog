1b78e6cb7ccebef43c249f8acf6bb67b19ac406f
Aleksandr Skripko <skripkoae@gmail.com>
2024-07-05 10:02:30 +0300

    request likes amount with Count() in Post.popular() (reduce 1 request for post page)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Comment, Post, Tag
+from django.db.models import Count
 
 
 def serialize_post(post):
@@ -41,7 +42,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
+    post = get_object_or_404(Post.objects.popular(), slug=slug)
     comments = post.comments.all().select_related('author')
     serialized_comments = []
     for comment in comments:
@@ -58,7 +59,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': post.likes.count(),
+        'likes_amount': post.likes_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
